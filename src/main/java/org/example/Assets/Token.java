374b83130c28cdc1010a42298a86105de90debd2
Abduroxman Qahramonov <83259854+Abduroxman-Qahramonov-04@users.noreply.github.com>
2023-06-06 21:28:01 +0500

    next change

---
+++ src/main/java/org/example/Assets/Token.java
@@ -0,0 +1,8 @@
+package org.example.Assets;
+
+public class Token {
+    private String token = "5905148412:AAGghg27ZK7vU4PBe6l0uHbMLaN39zMCs3o";
+    public String getToken(){
+        return this.token;
+    }
+}
